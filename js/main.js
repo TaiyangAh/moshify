b9ce9d9c207b53dc62079224c271057b4b68e346
TaiyangAh <41964050+TaiyangAh@users.noreply.github.com>
2022-01-30 18:34:23 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,3 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
-
-https://sub.xeton.dev/sub?target=clash&url=https%3A%2F%2Fs.sublink.me%2Fsubscribe%2F12306%2FURhOfSUxa88%2Fssr%2F&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini
